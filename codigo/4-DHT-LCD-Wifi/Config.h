8824042130589dbdad059dd95ee2277004e850fd
javacasm <javacasm@gmail.com>
2018-01-23 09:50:58 +0100

    update

---
+++ codigo/4-DHT-LCD-Wifi/Config.h
@@ -0,0 +1,9 @@
+//const char* ssid = "OpenWrt";
+const char* ssid = "JazzBajo";
+const char* password = "qazxcvbgtrewsdf";
+
+
+// Wifi movil
+//const char* ssid = "AAAAALJZvk4AQwFbMi5";
+//const char* password = "457C305C";
+
