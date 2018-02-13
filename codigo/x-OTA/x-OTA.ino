d72a5b7a63f91c324413b6b74c1977cac5083620
javacasm <javacasm@gmail.com>
2018-02-13 10:10:35 +0100

    config

--- codigo/x-OTA/x-OTA.ino
+++ codigo/x-OTA/x-OTA.ino
@@ -1,13 +1,16 @@
 /*
  * To start mDNS monitor (OSX) exec:   dns-sd -B _arduino._tcp
  */
+#include "Config.h"   // Configuración del wifi
 
 #include <JeVe_EasyOTA.h>  // https://github.com/jeroenvermeulen/JeVe_EasyOTA/blob/master/JeVe_EasyOTA.h
 
-#define WIFI_SSID        "xxxxx"
-#define WIFI_PASSWORD    "xxxxx"
+
 #define ARDUINO_HOSTNAME "ota-wemos-caldera"
 
+
+extern  char* ssid;
+extern  char* password;
 
 
 long blink_period;
@@ -20,7 +23,7 @@ void setup() {
   OTA.onMessage([](char *message, int line) {
     Serial.println(message);
   });
-  OTA.setup(WIFI_SSID, WIFI_PASSWORD, ARDUINO_HOSTNAME);
+  OTA.setup(ssid, password, ARDUINO_HOSTNAME);
 
   randomSeed(analogRead(0));
   blink_period=random(100,1000);
