49e721ec684db28242a76a70e8bef443f760ac72
javacasm <javacasm@gmail.com>
2018-02-14 23:16:21 +0100

    update

--- codigo/x-OTA-bmp180-chart/CircularBuffer.h
+++ codigo/x-OTA-bmp180-chart/CircularBuffer.h
@@ -6,13 +6,37 @@ struct CircularBuffer{
   int index=0;
   int maxValue=-32678;
   int minValue=32677;
+  
   void addValue(int iValue){
+    bool bRecalculateMin = false;
+    bool bRecalculateMax = false;
+    if ( data[index] == minValue ) bRecalculateMin = true;
+    if ( data[index] == maxValue ) bRecalculateMax = true;
     data[index] = iValue;
     index = ( index + 1 ) % NData ;
     if(maxValue<iValue) maxValue = iValue;
+    else if (bRecalculateMax ) {
+      for( int i = 0 ; i<NData ; i++ ){
+        if(maxValue<data[i]) maxValue = data[i];
+      }
+    }
     if(minValue>iValue) minValue = iValue;
+    else if (bRecalculateMin ) {
+      for( int i = 0 ; i<NData ; i++ ){
+        if(minValue>data[i]) minValue = data[i];
+      }
+    }
   }
+
   
+  
+  float getAverage(){
+    float sum = 0;
+    for( int i = 0 ; i<NData ; i++ ){
+      sum+=data[i];
+    }
+    return sum/NData;
+  }
   
   int getValue(int i){
     return data[ ( index + i ) % NData ] ;
