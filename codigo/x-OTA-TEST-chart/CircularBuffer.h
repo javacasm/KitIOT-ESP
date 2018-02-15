77d15d515a0f36c8915a18a037bf5795b2124d52
javacasm <javacasm@gmail.com>
2018-02-15 11:08:43 +0100

    test

--- codigo/x-OTA-TEST-chart/CircularBuffer.h
+++ codigo/x-OTA-TEST-chart/CircularBuffer.h
@@ -1,4 +1,5 @@
 
+// v2
 #define NMaxSize 500
 
 struct CircularBuffer{
@@ -6,19 +7,17 @@ struct CircularBuffer{
   int NData = 0;
   int iNextPosition = 0;
   int iFirstPosition = 0;
-  
+
   int maxValue = -32678;
   int minValue = 32677;
   long historicalCount = 0;
-  
+
   void addValue(int iValue){
-    bool bRecalculateMin = false;
-    bool bRecalculateMax = false;
 
     data[iNextPosition] = iValue;
-    
+
     iNextPosition = ( iNextPosition + 1 ) % NMaxSize ;
-    
+
     if ( NData < NMaxSize ) {
       NData = ( NData + 1 ) ;
     } else  {
@@ -34,20 +33,20 @@ struct CircularBuffer{
 
   int getMinimum(){
     minValue = getValue(0);
-    
-    for( int i = 1 ; i<NData ; i++ ) minValue = min(minValue, getValue(i) ); 
-     
+
+    for( int i = 1 ; i<NData ; i++ ) minValue = min(minValue, getValue(i) );
+
     return minValue ;
   }
 
   int getMaximum(){
     maxValue = getValue(0);
-    
-    for( int i = 1 ; i<NData ; i++ ) maxValue = max(maxValue, getValue(i) ); 
+
+    for( int i = 1 ; i<NData ; i++ ) maxValue = max(maxValue, getValue(i) );
 
     return maxValue;
   }
-  
+
   float getAverage(){
     float sum = 0;
     for( int i = 0 ; i<NData ; i++ ){
@@ -55,6 +54,6 @@ struct CircularBuffer{
     }
     return sum/NData;
   }
-  
+
 
 };
