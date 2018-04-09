40cb14fea5153afd12fbed8c567b78907d4d13ee
javacasm <javacasm@gmail.com>
2018-04-09 20:51:01 +0200

    update

--- codigo/ControlSimpleMotores/MotorShield.ino
+++ codigo/ControlSimpleMotores/MotorShield.ino
@@ -1,53 +1,16 @@
 #include "MotorShield.h"
 
-/* 
- *  
-    Motor Shield  NodeMCU DevKit  GPIO  Purpose
-    D1 PWMA (Motor A) D1  5 Speed
-    D3 DIRA (Motor A) D3  0 Direction
-    D2 PWMA (Motor B) D2  4 Speed
-    D4 DIRB (Motor B) D4  2 Direction
-
- */
-
-void setup_motorShield(){
-    pinMode(SPEED_MOTOR_A, OUTPUT); // 1,2EN aka D1 pwm left
-    pinMode(SPEED_MOTOR_B, OUTPUT); // 3,4EN aka D2 pwm right
-    pinMode(DIR_MOTOR_A, OUTPUT); // 1A,2A aka D3
-    pinMode(DIR_MOTOR_B, OUTPUT); // 3A,4A aka D4
+void setup_reles(){
+    pinMode(RELE_1, OUTPUT); 
+    pinMode(RELE_2, OUTPUT); 
 }
 
 
-void stop(void)
-{
-    analogWrite(SPEED_MOTOR_A, MIN_SPEED);
-    analogWrite(SPEED_MOTOR_B, MIN_SPEED);
+void apagaRele(int pinRele){
+    digitalWrite(pinRele, LOW);
 }
-void forward()
-{
-    analogWrite(SPEED_MOTOR_A, MAX_SPEED);
-    analogWrite(SPEED_MOTOR_B, MAX_SPEED);
-    digitalWrite(DIR_MOTOR_A, HIGH);
-    digitalWrite(DIR_MOTOR_B, HIGH);
+
+void enciendeRele(int pinRele){
+    digitalWrite(pinRele, HIGH);
 }
-void backward()
-{
-    analogWrite(SPEED_MOTOR_A, MAX_SPEED);
-    analogWrite(SPEED_MOTOR_B, MAX_SPEED);
-    digitalWrite(DIR_MOTOR_A, LOW);
-    digitalWrite(DIR_MOTOR_B, LOW);
-}
-void left(void)
-{
-    analogWrite(SPEED_MOTOR_A, MAX_SPEED);
-    analogWrite(SPEED_MOTOR_B, MAX_SPEED);
-    digitalWrite(DIR_MOTOR_A, LOW);
-    digitalWrite(DIR_MOTOR_B, HIGH);
-}
-void right()
-{
-    analogWrite(SPEED_MOTOR_A, MAX_SPEED);
-    analogWrite(SPEED_MOTOR_B, MAX_SPEED);
-    digitalWrite(DIR_MOTOR_A, HIGH);
-    digitalWrite(DIR_MOTOR_B, LOW);
-}
+
