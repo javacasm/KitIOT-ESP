f6625d40a5ca83ab022f4f75e7e289e03c58fe0e
Javacasm <javacasm@gmail.com>
2018-11-09 00:05:43 +0100

    update

--- codigo/x-OTA-bmp180-chart/x-OTA-bmp180-chart.ino
+++ codigo/x-OTA-bmp180-chart/x-OTA-bmp180-chart.ino
@@ -1,30 +1,12 @@
 // Wemos BMP180 shield https://github.com/wemos/D1_mini_Examples/blob/master/examples/03.Sensors/BMP180_BMP085/BMP180_BMP085.ino
 
-#include "Config.h"   // Configuración del wifi
 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+#include "Wifi.h"
 
-
-
-
-
-#include "Data.h"
 #include "WebServer.h"
+#include "Data.h"
 
-
-
-
-#include <JeVe_EasyOTA.h>  // https://github.com/jeroenvermeulen/JeVe_EasyOTA/blob/master/JeVe_EasyOTA.h
-
-
-
-
-
-
-
-extern  char* ssid;
-extern  char* password;
+#include "Indicator.h"
 
 long blink_period;
 long last_blink = millis();
@@ -32,66 +14,44 @@ long last_blink = millis();
 long data_period = 1000;
 long last_data = millis();
 
-
-
-
-
-
-
-EasyOTA OTA;
-
-
-
-
-
 void setup() {
   Serial.begin(9600);
-  // This callback will be called when JeVe_EasyOTA has anything to tell you.
-  OTA.onMessage([](char *message, int line) {
-    Serial.println(message);
-  });
-  OTA.setup(ssid, password, ARDUINO_HOSTNAME);
 
+  setup_indicator();
+
+  setup_Wifi();
+  
   // Para aleatorizar mas
   randomSeed(analogRead(0));
 
   // definimos el periodo de parpadeo
   blink_period=random(1000,2000);
 
-  pinMode(LED_BUILTIN,OUTPUT);
-
   setup_Sensor();
-
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
 
   setup_webserver();
 }
 
 
 
-
-
 void loop() {
 
-  // OTA
-  OTA.loop();
+  loop_wifi();
 
   // Blink
   if(millis()-last_blink>blink_period){
-    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+    blinkIndicator();
     last_blink=millis();
   }
 
   // Data
   if(millis()-last_data>data_period){
-    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+    blinkIndicator();
     getData();
     formatData();
     showData();
     last_data=millis();
-   // digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+   // blinkIndicator();
   }
 
  loop_webserver();
