a8e2b0ad38c560e7d80e9529726448b6301128d3
Javacasm <javacasm@gmail.com>
2018-11-08 11:56:28 +0100

    update

--- codigo/x-OTA-bmp180-chart/x-OTA-bmp180-chart.ino
+++ codigo/x-OTA-bmp180-chart/x-OTA-bmp180-chart.ino
@@ -4,23 +4,24 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 
-#include <Wire.h>
-#include <Adafruit_BMP085.h>
 
-#include "CircularBuffer.h"
 
-Adafruit_BMP085 bmp;
+
+
+#include "Data.h"
+#include "WebServer.h"
+
+
 
 
 #include <JeVe_EasyOTA.h>  // https://github.com/jeroenvermeulen/JeVe_EasyOTA/blob/master/JeVe_EasyOTA.h
 
-#define ARDUINO_HOSTNAME "OTA-nodeMCU_Priego-test"
 
-#define MAX_PAGE_LENGTH 1000
 
-ESP8266WebServer server(80);
+
+
+
 
 extern  char* ssid;
 extern  char* password;
@@ -28,16 +29,20 @@ extern  char* password;
 long blink_period;
 long last_blink = millis();
 
-long data_period = 500;
+long data_period = 1000;
 long last_data = millis();
 
-CircularBuffer TempData;
 
-float temp, preassure, altitude;
 
-String strTemp, strPrea, strAlt;
+
+
+
 
 EasyOTA OTA;
+
+
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -51,148 +56,22 @@ void setup() {
   randomSeed(analogRead(0));
 
   // definimos el periodo de parpadeo
-  blink_period=random(100,1000);
+  blink_period=random(1000,2000);
 
   pinMode(LED_BUILTIN,OUTPUT);
 
-  if (!bmp.begin()) {
-    Serial.println("Could not find BMP180 or BMP085 sensor at 0x77");
-    while (1) {
-      digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
-      Serial.println("Could not find BMP180 or BMP085 sensor at 0x77");
-      delay(50);
-     }
-  }
+  setup_Sensor();
 
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-     server.on("/", handleRoot);
-  server.on ( "/test.svg", drawGraph );
-  server.on("/inline", [](){
-    server.send(200, "text/plain", getRootPage());
-  });
+  
 
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
-}
-
-char pageBuffer[MAX_PAGE_LENGTH];
-String strTitle = "BMP180 Datos atmosfericos";
-const char * getRootPage(){
-
-  String strBody = strTemp + "<br>" +
-                    strPrea + "<br>" +
-                    strAlt;
-
-
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-
-  snprintf ( pageBuffer, MAX_PAGE_LENGTH,
-
-"<html>\
-  <head>\
-    <meta http-equiv='refresh' content='1'/>\
-    <title>%s</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    %s\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <p>Medidas: %d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-
-    strTitle.c_str(),strBody.c_str(),hr, min % 60, sec % 60,TempData.historicalCount
-  );
-
-  return pageBuffer;
+  setup_webserver();
 }
 
 
 
-String getChart(CircularBuffer *data,String strColor){
-  String out = "";
-  char temp[100];
-  int iWidth = 400 ;
-  int iHeight = 140 ;
-  int iMargin = 10 ;
-  sprintf(temp , "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"%d\" height=\"%d\">\n",iWidth,iHeight);
-  out += temp;
-  sprintf(temp , "<rect width=\"%d\" height=\"%d\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n",iWidth,iHeight);
-  out += temp;
-  out += "<g stroke=\""+strColor+"\">\n";
-  int y = map(data->getValue(0),data->getMinimum(),data->getMaximum(),0,iHeight - iMargin) + iMargin / 2 ;
-  float step = ( iWidth - 2.0 * iMargin ) / data->NData ;
-  for (float x = iMargin, i = 1; i < data->NData; x += step, i++) {
-    int y2 = map(data->getValue(i),data->minValue,data->maxValue,0,iHeight - iMargin) + iMargin / 2 ;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", int(x), iHeight - y, int(x + step), iHeight - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-  return out;
-}
 
-void drawGraph() {
-
-
-  server.send ( 200, "image/svg+xml", getChart(&TempData,"blue"));
-}
-
-void handleRoot() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  server.send(200, "text/HTML", getRootPage());
-  digitalWrite(LED_BUILTIN, LOW);
-}
-
-void handleNotFound(){
-  digitalWrite(LED_BUILTIN, HIGH);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/HTML", message);
-    digitalWrite(LED_BUILTIN, LOW);
-}
-
-// Get data from the sensor
-void getData(){
-  temp = bmp.readTemperature();
-  preassure = bmp.readPressure();
-  // Calculate altitude assuming 'standard' barometric
-  // pressure of 1013.25 millibar = 101325 Pascal
-  altitude =  bmp.readAltitude();
-
-  TempData.addValue(int(temp*5));
-}
-
-// Convert numerical to text data
-void formatData(){
-  strTemp = String("Temperatura = ") + temp + " C";
-  strPrea = String("Presion = ") + preassure + " Pa";
-  strAlt = String("Altitud = ")+altitude + " metros";
-}
-
-// Show data over serial
-void showData(){
-  Serial.println(strTemp);
-  Serial.println(strPrea);
-  Serial.println(strAlt);
-}
 
 void loop() {
 
@@ -212,10 +91,9 @@ void loop() {
     formatData();
     showData();
     last_data=millis();
-    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+   // digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
   }
 
-  // Webserver
-  server.handleClient();
+ loop_webserver();
 
 }
